60439728e187d9aacb2770b60e829de34ccc5dc7
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-03-23 18:00:50 +0100

    Add left right buttons with states

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,40 +1,71 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ handleClick, text}) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-  const increaseByOne = () => setCounter(counter + 1)
-
-  const setToZero = () => setCounter(0)
-
-  const decreaseByOne = () => setCounter(counter - 1)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+  
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button text='plus' handleClick={increaseByOne} />
-      <Button text='zero' handleClick={setToZero} />
-      <Button text='minus' handleClick={decreaseByOne} />
+      <div>
+        {left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {right}
+        <p>{allClicks.join(' ')}</p>
+      </div>
     </div>
   )
 }
-
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 )
 
+// COUNTER APP CODE
+//
+// const Display = ({ counter }) => <div>{counter}</div>
 
+// const Button = ({ handleClick, text}) => (
+//   <button onClick={handleClick}>
+//     {text}
+//   </button>
+// )
+
+// const App = () => {
+//   const [ counter, setCounter ] = useState(0)
+
+//   const increaseByOne = () => setCounter(counter + 1)
+
+//   const setToZero = () => setCounter(0)
+
+//   const decreaseByOne = () => setCounter(counter - 1)
+
+//   return (
+//     <div>
+//       <Display counter={counter} />
+//       <Button text='plus' handleClick={increaseByOne} />
+//       <Button text='zero' handleClick={setToZero} />
+//       <Button text='minus' handleClick={decreaseByOne} />
+//     </div>
+//   )
+// }
+
+// GREETINGS APP CODE
+//
 // const Hello = ({ name, age }) => {
 //   const bornYear = () => new Date().getFullYear() - age
   
