4f7b3966c02b106ef61c60ca74f17b169ba9aa00
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-03-23 17:32:49 +0100

    Add state hook

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,28 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
-  
-  return (
-    <>
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born {bornYear()}</p>
-    </>
-  )
-}
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
-  const nimi = 'Pekka'
-  const ika = 29
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+
+  const setToZero = () => setCounter(0)
+
+  const decreaseByOne = () => setCounter(counter - 1)
 
   return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={nimi} age={ika} />
-    </>
+    <div>
+      <Display counter={counter} />
+      <Button text='plus' handleClick={increaseByOne} />
+      <Button text='zero' handleClick={setToZero} />
+      <Button text='minus' handleClick={decreaseByOne} />
+    </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
+
+// const Hello = ({ name, age }) => {
+//   const bornYear = () => new Date().getFullYear() - age
+  
+//   return (
+//     <>
+//       <p>Hello {name}, you are {age} years old</p>
+//       <p>So you were probably born {bornYear()}</p>
+//     </>
+//   )
+// }
+
+// const App = () => {
+//   const nimi = 'Pekka'
+//   const ika = 29
+
+//   return (
+//     <>
+//       <h1>Greetings</h1>
+//       <Hello name="Maya" age={26 + 10} />
+//       <Hello name={nimi} age={ika} />
+//     </>
+//   )
+// }
