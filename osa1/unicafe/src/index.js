0c8f51c6f3e3186a1b8f46c5b004bb375ee8aaa1
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-03-24 11:51:29 +0100

    Finish unicafe part 1

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,14 +1,42 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = ({ text }) => <h1>{text}</h1>
+const Subheader = ({ text }) => <h2>{text}</h2>
+
+const Button = ({ text, handleClick }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
+  if (!(good || neutral || bad)) return <p>No feedback given</p>
+
+  return (
+    <>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const header = "Give feedback"
+  const subheader = "Statistics"
 
   return (
     <div>
-      //code here
+      <Header text={header} />
+      <Button text='good' handleClick={() => setGood(good + 1)} />
+      <Button text='neutral' handleClick={() => setNeutral(neutral + 1)} />
+      <Button text='bad' handleClick={() => setBad(bad + 1)} />
+      <Subheader text={subheader} />
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
