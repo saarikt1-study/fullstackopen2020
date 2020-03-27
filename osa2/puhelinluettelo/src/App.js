c427441f94e488dac8be679e521a1d9216c2c7fa
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-03-27 11:29:22 +0100

    Finish puhelinluettelo step1

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,24 +1,51 @@
 import React, { useState } from 'react'
 
+const Line = (props) => {
+  return (
+    <p>{props.name}</p>
+  )
+}
+
 const App = () => {
   const [ persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const handleNameChange = (event) => {
+    setNewName(event.target.value) 
+  }
+
+  const addLine = (event) => {
+    event.preventDefault()
+    const lineObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(lineObject))
+    setNewName('')
+  }
+  
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addLine}>
         <div>
-          name: <input />
+          name: <input 
+                  value={newName}
+                  onChange={handleNameChange}
+                />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map((person) =>
+          <Line key={person.name} name={person.name} />
+        )}
+      </div>
     </div>
   )
 
