0c04a15c3e20bb817141cf50a9e9a832ab2c8ea4
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-04-02 10:41:42 +0200

    Add error message

--- misc/part2/src/App.js
+++ misc/part2/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
+import Notification from './components/Notification'
 import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('Some error might\'ve happened...')
 
   useEffect(() => {
     noteService
@@ -49,9 +51,12 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
     })
     .catch(error => {
-      alert (
-        `The note '${note.content}' was already deleted from the server`
+      setErrorMessage(
+        `Note '${note.content}' was already removed from the server`
       )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
       setNotes(notes.filter(n => n.id !== id))
     })
 
@@ -60,6 +65,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
