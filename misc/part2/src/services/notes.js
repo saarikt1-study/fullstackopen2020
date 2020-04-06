13c891536294324bc49c07829a5d82ea0b85f2c7
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-04-06 13:07:27 +0200

    Change url to Heroku

--- misc/part2/src/services/notes.js
+++ misc/part2/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = 'https://agile-reaches-55195.herokuapp.com/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
