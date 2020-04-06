f6ffa91d8b2a6ca4cef394f3eeacab541fa1e439
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-04-06 11:06:01 +0200

    Add procfile

--- misc/part3/index.js
+++ misc/part3/index.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
 app.use(express.json())
+app.use(requestLogger)
+app.use(cors())
 
 let notes = [
   {
@@ -21,12 +32,40 @@ let notes = [
     content: "GET and POST are the most important methods of HTTP protocol",
     date: "2020-01-10T19:20:14.298Z",
     important: true
+  },
+  {
+    id: 4,
+    content: "Testi!!!",
+    date: "2020-01-10T19:20:14.298Z",
+    important: true
   }
 ]
 
+const generateId = () => {
+  const maxId = notes.length > 0
+  ? Math.max(...notes.map(n => n.id))
+  : 0
+
+  return maxId + 1
+}
+
 app.post('/api/notes', (request, response) => {
-  const note = request.body
-  console.log(note)
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  }
+  
+  notes = notes.concat(note)
 
   response.json(note)
 })
@@ -56,7 +95,13 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
